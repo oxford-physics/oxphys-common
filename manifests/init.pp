57c9f169de399429f6467f56f2b067bb902e35ee
kashif74 <kashif.alig@gmail.com>
2015-09-14 15:48:20 +0100

    NTP altered in response to ext. service alteration

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,10 @@
-class common {
-   $package_list = [ "screen", "vim-enhanced", "strace", "mlocate", "tree", "ntp", "augeas" ,"telnet","ruby-json", "ipmitool", "jwhois"]
+class common (
+   $ntpservers = hiera("common::ntpservers", ["ntp0.physics.ox.ac.uk", "ntp1.physics.ox.ac.uk", "ntp1.oucs.ox.ac.uk" ])
+)
+ {
+#   $package_list = [ "screen", "vim-enhanced", "strace", "mlocate", "tree", "ntp", "augeas" ,"telnet","ruby-json", "ipmitool", "jwhois" , "iperf" ]
+#  XXX Remove ruby-json as it doesn't exist on EL7 - Ewan 20150625
+   $package_list = [ "screen", "vim-enhanced", "strace", "mlocate", "tree", "ntp", "augeas" ,"telnet", "ipmitool", "jwhois" , "iperf" ]
    package { $package_list : ensure => installed, tag=>"basepackage"}
    
 #   ensure_packages ( ['perf'] )
@@ -21,12 +26,20 @@ class common {
 #    subscribe  => File['/etc/puppet/puppet.conf'], 
 #     }
   
+    file { '/root/.vimrc':
+    ensure  => present,
+    mode       => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source => "puppet:///modules/common/vimrc",
+     }
+
     file { '/etc/ntp.conf':
     ensure  => present,
     mode       => '0644',
     owner   => 'root',
     group   => 'root',
-    source => "puppet:///modules/common/ntp.conf",
+    content => template("common/ntp.conf.erb"),
     require => Package['ntp'],
      }
    
